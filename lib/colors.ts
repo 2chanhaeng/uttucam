e39b0d09e604bf0ae11a84368d14f277adf2a700
ChanHaeng Lee <2chanhaeng@gmail.com>
2025-04-10 12:09:25 +0900

    feat: define colors

---
+++ lib/colors.ts
@@ -0,0 +1,20 @@
+export const colors = [
+  "red",
+  "orange",
+  "amber",
+  "yellow",
+  "lime",
+  "green",
+  "emerald",
+  "teal",
+  "cyan",
+  "sky",
+  "blue",
+  "indigo",
+  "violet",
+  "purple",
+  "fuchsia",
+  "pink",
+  "rose",
+] as const;
+export type ColorOption = typeof colors[number];
