e1d5dcbbc7ed8d4d1889947e2247fa14042c4a16
ChanHaeng Lee <2chanhaeng@gmail.com>
2025-04-17 18:41:17 +0900

    feat: change default model

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,4 +1,4 @@
-import { google } from "@ai-sdk/google";
+import { openai } from "@ai-sdk/openai";
 import { streamText } from "ai";
 
 // Allow streaming responses up to 30 seconds
@@ -6,10 +6,10 @@ export const maxDuration = 30;
 
 export async function POST(req: Request) {
   const { messages /* , modelId */ } = await req.json();
-  const modelId = "gemini-2.5-pro-exp-03-25";
+  const modelId = "o4-mini-2025-04-16";
 
   const result = streamText({
-    model: google(modelId),
+    model: openai(modelId),
     messages,
   });
 
