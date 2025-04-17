60baa000b6ceb3b285ed14bfe6159d2d09f7d923
ChanHaeng Lee <2chanhaeng@gmail.com>
2025-04-17 18:36:10 +0900

    feat: wrap Phase3 with section

--- components/chat-start-form/phase3.tsx
+++ components/chat-start-form/phase3.tsx
@@ -1,5 +1,9 @@
 import ReactMarkdown from "react-markdown";
 
 export default function Phase3({ advice }: { advice: string }) {
-  return <ReactMarkdown>{advice}</ReactMarkdown>;
+  return (
+    <section className="flex flex-col gap-2 p-4 items-start">
+      <ReactMarkdown>{advice}</ReactMarkdown>
+    </section>
+  );
 }
