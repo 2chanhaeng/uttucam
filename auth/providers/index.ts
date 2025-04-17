79a9bc3e72656d87d14cf5f085679e4809311d47
ChanHaeng Lee <2chanhaeng@gmail.com>
2025-04-17 23:31:34 +0900

    refac: remove unused code

--- auth/providers/index.ts
+++
@@ -1,7 +0,0 @@
-import { Provider } from "next-auth/providers";
-import GitHub from "next-auth/providers/github";
-import Discord from "next-auth/providers/discord";
-
-const providers: Provider[] = [GitHub, Discord];
-
-export default providers;
